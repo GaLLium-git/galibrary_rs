1857f2466d02fbb8eb8d7018387c1c04f748d936
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-06-12 21:01:49 +0900

    Update cumulate.rs

--- algo/cumulate.rs
+++ algo/cumulate.rs
@@ -8,6 +8,15 @@ pub fn cumulate<T:Copy,F:Fn(T,T)->T>(A:&Vec<T>,f:F) -> Vec<T>{
 
 pub fn cumulate2D<T:Copy,F:Fn(T,T)->T>(A:&Vec<Vec<T>>,f:F) -> Vec<T>{
     let mut res = A.clone();
-    
+    for i in 0..A.len(){
+        for j in 1..A[0].len(){
+            res[i][j] = f(res[i][j],res[i][j-1]);
+        }
+    }
+    for j in 0..A[0].len(){
+        for j in 1..A.len(){
+            res[i][j] = f(res[i][j],res[i-1][j]);
+        }
+    }
     res
 }
