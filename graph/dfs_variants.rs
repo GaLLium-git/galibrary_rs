f75f7cfe0577e02f5af650f7de5e9739dce1a23a
GaLLium-git <205247616+GaLLium-git@users.noreply.github.com>
2026-07-29 13:02:24 +0900

    Update dfs_variants.rs

--- graph/dfs_variants.rs
+++ graph/dfs_variants.rs
@@ -5,7 +5,7 @@ pub fn dfs(graph:&Vec<Vec<usize>>, v:usize, seen:&mut Vec<bool>){
     seen[v]=true;
     for &nv in graph[v].iter(){
         if !(seen[nv]) {
-          dfs(nv,graph,seen);
+          dfs(graph,nv,seen);
         }
     }
 }
@@ -14,7 +14,7 @@ pub fn dfs(graph:&Vec<Vec<usize>>, v:usize, seen:&mut Vec<bool>){
 pub fn dfs(graph:&Vec<Vec<usize>>, v:usize, p:usize){
     for &nv in graph[v].iter(){
         if nv == p {continue;}
-        dfs(nv,graph,seen);
+        dfs(graph,nv,v);
     }
 }
 
